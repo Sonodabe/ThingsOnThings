88aef7e3e4aba9d1c6862457dcef1f7bd6dcdd2c
Brandon Sonoda <sonodabe@muohio.edu>
2012-09-20 17:56:11 -0400

    Git ignore

--- xcode/build/ThingsOnThings.build/Debug/ThingsOnThings.build/Objects-normal/i386/List.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/brandonsonoda/Documents/College/SemesterThree/CSE\ 274/ThingsOnThings/xcode/List.cpp \
-  /Users/brandonsonoda/Documents/College/SemesterThree/CSE\ 274/ThingsOnThings/xcode/Node.h \
-  /Users/brandonsonoda/Documents/College/SemesterThree/CSE\ 274/ThingsOnThings/xcode/List.h
